0b19ab5e0be0f3785f097ffa84ff163e0c84e485
Logan Garrett <lngarrett@gmail.com>
2015-11-18 20:50:03 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ docker pull lngarrett/docker-dnsmasq:latest
 docker run \
   --name="dnsmasq" \
   --publish=53:53 \
-  --net=host
+  --net=host \
   --restart="always" \
   --detach=true \
   lngarrett/docker-dnsmasq
